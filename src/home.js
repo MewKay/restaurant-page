f54f3cac2ddfc78acaa8024dd89a4e866cbb165f
MewKay <125735249+MewKay@users.noreply.github.com>
2023-07-10 02:38:03 +0300

    Add header the homepage

--- src/home.js
+++ src/home.js
@@ -1,8 +1,40 @@
 const Homepage = () => {
   const homeContainer = document.createElement("div");
   homeContainer.id = "home";
+
+  homeContainer.appendChild(Header());
   
   return homeContainer;
+}
+
+const Header = () => {
+  const headerContainer = document.createElement("header");
+  
+  const addBrandName = () => {    
+    const brandName = document.createElement("h1");
+    brandName.innerText = "My Pizzeria";
+    brandName.classList.add("brand-name");
+    headerContainer.appendChild(brandName);
+  }
+  
+  const addButtonsSection = () => {
+    const buttonsSection = document.createElement("div");
+    buttonsSection.classList.add("btn-section");
+    const menuButton = document.createElement("button");
+    const contactButton = document.createElement("button");
+    menuButton.classList.add("btn-menu");
+    menuButton.innerText = "Menu";
+    buttonsSection.appendChild(menuButton);
+    contactButton.classList.add("btn-contact");
+    contactButton.innerText = "Contact";
+    buttonsSection.appendChild(contactButton);
+    headerContainer.appendChild(buttonsSection);
+  }
+
+  addBrandName();
+  addButtonsSection();
+
+  return headerContainer;
 }
 
 export {
