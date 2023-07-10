b4e7250652910ecddd0c920abe616716938864e8
MewKay <125735249+MewKay@users.noreply.github.com>
2023-07-11 00:58:01 +0300

    Add the barebone of the Menu page

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import "./reset.css";
 import "./style.css";
 import { Homepage } from "./home.js";
+import { MenuPage } from "./menu";
 
 const displayPage = () => {
   const container = document.querySelector("#content");
---
+++ src/menu.js
@@ -0,0 +1,10 @@
+const MenuPage = () => {
+  const menuContainer = document.createElement("div");
+  menuContainer.id = "menu";
+
+  return menuContainer;
+}
+
+export {
+  MenuPage
+};
