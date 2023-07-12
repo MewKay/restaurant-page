6103cceaf0a22c4a4d74b26032848a45ad3bf00b
MewKay <125735249+MewKay@users.noreply.github.com>
2023-07-13 02:27:58 +0300

    Add informations to each menu items

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -43,10 +43,49 @@ const createMenuList = () => {
 }
 
 const addMenuItemsToList = (container) => {
+  let menuItems = [];
   for (let i=0; i<6; i++) {
     const menuItem = createMenuItem(i);
-    container.appendChild(menuItem.getNode());
+    menuItems.push(menuItem);
+    container.appendChild(menuItems[i].getNode());
   }
+  
+  menuItems[0].addItemName("Vegetarian");
+  menuItems[0].addItemImageCredit(
+    "Laure Noverraz",
+    "Unsplash",
+    "https://unsplash.com/fr/s/photos/veggie-pizza?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText");
+    
+  menuItems[1].addItemName("Margherita");
+  menuItems[1].addItemImageCredit(
+    "KamranAydinov",
+    "Freepik",
+    "https://www.freepik.com/free-photo/pizza-margarita-table_6087619.htm#query=margherita&position=0&from_view=search&track=sph");
+
+  menuItems[2].addItemName("Sicilian");
+  menuItems[2].addItemImageCredit(
+    "Parker Hilton",
+    "Unsplash",
+    "https://unsplash.com/fr/s/photos/Pizza-sicilienne?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText");
+
+  menuItems[3].addItemName("Hawaiian");
+  menuItems[3].addItemImageCredit(
+    "stockgiu",
+    "Freepik",
+    "https://www.freepik.com/free-ai-image/pizza-with-pineapple-it_43023563.htm#query=margherita&position=13&from_view=search&track=sph");
+
+  menuItems[4].addItemName("Pepperoni");
+  menuItems[4].addItemImageCredit(
+    "azerbaijan_stockers",
+    "Freepik",
+    "https://fr.freepik.com/photos-gratuite/tranche-pizza-au-pepperoni-classique-rouleaux-poivron-vert_5449364.htm#query=pizza%20aux%20pepperoni&position=1&from_view=search&track=ais");
+
+  menuItems[5].addItemName("Quattro Formaggi");
+  menuItems[5].addItemImageCredit(
+    "KamranAydinov",
+    "Freepik",
+    "https://fr.freepik.com/photos-gratuite/pizza-quatre-fromages-table_7218711.htm#query=pizza%20aux%204%20fromages&position=24&from_view=keyword&track=ais");
+
 }
 
 const createMenuItem = (index) => {
