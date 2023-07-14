74f14f2bf36b7bb8e53927bb594f3c280b95bbd6
MewKay <125735249+MewKay@users.noreply.github.com>
2023-07-14 23:21:32 +0300

    Add contact informations

--- src/modules/contact.js
+++ src/modules/contact.js
@@ -28,6 +28,32 @@ const createContactContent = () => {
   const contactContent = document.createElement("div");
   contactContent.id = "contact-content";
 
+  const createContactInfos = (() => {
+    const contactInfoContainer = document.createElement("div");
+    contactInfoContainer.id = "contact-info";
+
+    const phoneNumberSection = document.createElement("section");
+    const phoneNumberSectionTitle = document.createElement("h2");
+    phoneNumberSectionTitle.innerText = "Phone Number";
+    const phoneNumber = document.createElement("p");
+    phoneNumber.innerText = "XXX-XXX-XXXX";
+    phoneNumberSection.appendChild(phoneNumberSectionTitle);
+    phoneNumberSection.appendChild(phoneNumber);
+
+    const addressSection = document.createElement("section");
+    const addressSectionTitle = document.createElement("h2");
+    addressSectionTitle.innerText = "Address";
+    const addressName = document.createElement("p");
+    addressName.innerText = "Somewhere-that-smells-good Boulevard";
+    addressSection.appendChild(addressSectionTitle);
+    addressSection.appendChild(addressName);
+
+    contactInfoContainer.appendChild(phoneNumberSection);
+    contactInfoContainer.appendChild(addressSection);
+    
+    contactContent.appendChild(contactInfoContainer);
+  })();
+
   return contactContent;
 }
 
